fc5f3b101a82a94ec3a0afb6330939f6f1fa0a1e
McabralDS <mateuscabralmc@hotmail.com>
2025-03-02 21:17:31 -0300

    Add initial layout, styles, and dependencies for Venus project

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+  <script setup lang="ts">
+    // import '~/assets/theseasons-bd.otf'
+  
+  </script>
+
+<template>
+<div class="w-screen h-screen flex  justify-center  bg-gradient-to-b from-white to-violet-400 p-4">
+  <span>
+    <h1 class="text-indigo-800 text-6xl">Chá da Venus</h1>
+  </span>
+  <Slot/>
+
+  
+
+</div>
+</template>
+
+
+<style scoped>
+
+h1 {
+  font-family: 'Theseasons';
+  /* color: #ffe7a9; */
+}
+
+
+
+</style>
