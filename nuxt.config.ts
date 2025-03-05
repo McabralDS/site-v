0f91351b1169df3eff87bfd18b6815c369fb7cb3
McabralDS <mateuscabralmc@hotmail.com>
2025-03-04 23:16:15 -0300

    Add theme color meta tag in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        { name: 'theme-color', content: '#8b5cf6' }
       ],
       link: [
         // Seus links, como fontes etc.
